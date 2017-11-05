5ed60fd5304cc1975bd42d5d083f229f4bc2e19a
Luke Stewart <lukestewart@gmail.com>
2017-11-04 22:22:07 -0700

    homework

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+//Display the dynamic interface; Title, Instructions, Wins, Losses, Guesses left, Letters guessed.
+
+
+//computer picks a random letter
+
+
+//store players guess in a variable
+
+
+//compare variable to computer's pick
+
+
+//if correct, add 1 to wins and reset game
+
+
+//if wrong, subtract 1 from total guesses and display guessed letter
+
+
+//if guesses reach 0, add 1 to Losses and reset game
